bf19834c9bc632d453eaabeed11f96c58c3ea71b
covingtonhighunblocked1 <covingtonhighunblocked@gmail.com>
2021-12-14 19:44:16 +0000

    add n64

---
+++ N64Wasm/dist/romlist.js
@@ -0,0 +1,11 @@
+var ROMLIST = [
+    {url: "roms/BK64.n64", title: "Banjo-Kazooie 64"},
+    {url: "roms/PM64.n64", title: "Paper Mario 64"},
+    {url: "roms/SM64.n64", title: "Super Mario 64"},
+    {url: "roms/TLOZ-OoT.n64", title: "The Legend of Zelda - Ocarina Of Time"},
+    /*
+    {url:"roms/rom1.z64",title:"Game 1"},
+    {url:"roms/rom2.v64",title:"Game 2"},
+    {url:"roms/rom3.v64",title:"Game 3"},
+    */
+];
