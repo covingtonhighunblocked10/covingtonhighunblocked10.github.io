c7b9bf1354f98727c38d319d86e2942dc33111fc
covington-coding <covington.coding@gmail.com>
2022-03-28 14:06:08 +0000

    push dono

---
+++ myProjects/ray-casting/index.js
@@ -0,0 +1,18 @@
+$(document).ready(runRaycast)
+
+function runRaycast() {
+
+    //declare canvas drawing
+    var c = document.getElementById("canvas")
+    var ctx = c.getContext("2d");
+
+    var level = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ]
+}
